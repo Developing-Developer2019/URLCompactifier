924a889a5015ba4913266b8463c8b78756abad63
Marvin Brereton <marvin.brereton@gmail.com>
2022-03-22 17:29:13 +0000

    Add project files.

---
+++ URLCompactifier/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace URLCompactifier.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
