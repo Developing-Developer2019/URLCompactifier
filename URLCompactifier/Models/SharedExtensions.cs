c096bccc202945e44678a2e73e6550d658d0e14f
Marvin Brereton <marvin.brereton@gmail.com>
2022-03-22 23:35:15 +0000

    Added shared extension

---
+++ URLCompactifier/Models/SharedExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace URLCompactifier.Models.SharedExtensions
+{
+    /// <summary>
+    /// Shared extension class
+    /// </summary>
+    public static class SharedExtensions
+    {
+        /// <summary>
+        /// Input Https to allow redirects easier
+        /// </summary>
+        /// <param name="value">string to check</param>
+        public static string HttpInput(string value)
+        {
+            if (!value.Contains("https://"))
+            {
+                value = $"https://{value}";
+            }
+
+            return value;
+        }
+    }
+}
