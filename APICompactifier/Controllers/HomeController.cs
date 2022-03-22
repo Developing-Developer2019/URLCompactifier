7c0430f8f241a7668bd2771dad75f96bf5be2c55
Marvin Brereton <marvin.brereton@gmail.com>
2022-03-22 23:44:17 +0000

    Revert "Removed extra API project as not needed"
    This reverts commit c46205679f9cc01985af39a7656beec273a09d39.

---
+++ APICompactifier/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿//using Microsoft.AspNetCore.Mvc;
+//using URLCompactifier.Models;
+
+//namespace APICompactifier.Controllers
+//{
+//    public class URLResponse
+//    {
+//        public string urlLink { get; set; }
+//        public string statusLink { get; set; }
+//        public string tokenLink { get; set; }
+//    }
+//    public class HomeController : Controller
+//    {
+//        // Our index Route
+//        [HttpGet, Route("/")]
+//        public IActionResult Index()
+//        {
+//            return View();
+//        }
+
+//        // Our URL shorten route
+//        [HttpPost, Route("/")]
+//        public IActionResult PostURL([FromBody] string url)
+//        {
+//            // Initiate variables
+//            var linkArray = url.Split(new[] { '/' }, 2);
+
+//            // If the url does not contain HTTP prefix it with it
+//            if (!url.Contains("http"))
+//            {
+//                url = "http://" + url;
+//            }
+
+//            if (SqlLiteDataAccess.DoesTokenExist(linkArray[1]))
+//            {
+//                LinkDTO linkDTO = new LinkDTO();
+//                linkDTO.SecondaryLink = SqlLiteDataAccess.GetSecondaryLink(linkArray[1]);
+//                linkDTO.PrimaryLink = SqlLiteDataAccess.GetPrimaryLink(linkDTO.SecondaryLink.PrimaryLink_ID);
+
+//                FindRedirect(url);
+//            }
+//        }
+
+//        private string FindRedirect(string url)
+//        {
+//            string result = string.Empty;
+//            using (var client = new HttpClient())
+//            {
+//                var response = client.GetAsync(url).Result;
+//                if (response.IsSuccessStatusCode)
+//                {
+//                    result = response.Headers.Location.ToString();
+//                }
+//            }
+//            return result;
+//        }
+//    }
+//}
